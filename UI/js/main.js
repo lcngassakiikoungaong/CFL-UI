a8b708c0917cfd19327409b06523ede9faf550a6
Jonty <46462272+WhoTookJonty@users.noreply.github.com>
2022-10-20 00:43:12 -0400

    added file structure and a new js file for adding functionaility to index.html

---
+++ UI/js/main.js
@@ -0,0 +1,29 @@
+"use strict"; //execute in strict mode
+
+let homeNav = document.getElementById("homeNav");
+let aboutNav = document.getElementById("aboutNav");
+let contactNav = document.getElementById("contactNav");
+let blogNav = document.getElementById("blogNav");
+let heroBtn = document.getElementById('libertyBtn')
+
+
+homeNav.onclick = function ()
+{
+   
+}
+aboutNav.onclick = function ()
+{
+    
+}
+contactNav.onclick = function ()
+{
+    
+}
+blogNav.onclick = function ()
+{
+    
+}
+libertyBtn.onclick = function ()
+{
+    
+}
